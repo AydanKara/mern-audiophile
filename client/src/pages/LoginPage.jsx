0d3db6fd8341646dcf089a60cf5eb7555713063f
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-09-11 11:31:56 +0200

    complete sign up page functionality

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,59 +1,82 @@
 import { Link } from "react-router-dom";
 import "../styles/auth.css";
 import "../styles/form.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AuthContext from "../context/authContext";
 import useLoginForm from "../hooks/useLoginForm";
 
 const LoginPage = () => {
-  const { loginSubmitHandler } = useContext(AuthContext);
+  /*   const { loginSubmitHandler } = useContext(AuthContext);
   const { values, errors, serverError, onChange, onSubmit } =
-    useLoginForm(loginSubmitHandler);
+    useLoginForm(loginSubmitHandler); */
+
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+    console.log(data);
+  };
   return (
     <>
       <div className="site-heading">
         <h1 className="heading-title">Login</h1>
       </div>
-      {serverError && (
+      {/*  {serverError && (
         <div className="alert">
           <h2>{serverError}</h2>
         </div>
-      )}
-      <form onSubmit={onSubmit}>
+      )} */}
+      <form onSubmit={handleSubmit}>
         <p>
           <label htmlFor="email">E-Mail</label>
-          {errors.email && <span className="error">{errors.email}</span>}
+          {/* {errors.email && <span className="error">{errors.email}</span>} */}
           <input
             type="email"
             name="email"
             id="email"
-            onChange={onChange}
-            value={values?.email}
-            className={
+            onChange={handleChange}
+
+            /* className={
               errors.email
                 ? "error-input"
                 : "" || serverError
                 ? "error-input"
                 : ""
-            }
+            } */
           />
         </p>
         <p>
           <label htmlFor="password">Password</label>
-          {errors.password && <span className="error">{errors.password}</span>}
+          {/* {errors.password && <span className="error">{errors.password}</span>} */}
           <input
             type="password"
             name="password"
             id="password"
-            onChange={onChange}
-            value={values?.password}
-            className={
+            onChange={handleChange}
+            /* className={
               errors.password
                 ? "error-input"
                 : "" || serverError
                 ? "error-input"
                 : ""
-            }
+            } */
           />
         </p>
         <button type="submit" className="btn-1">
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,80 +1,121 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../styles/auth.css";
 import "../styles/form.css";
-import { useContext } from "react";
-import AuthContext from "../context/authContext";
-import useRegisterForm from "../hooks/useRegisterForm";
+import { useState } from "react";
+/* import AuthContext from "../context/authContext";
+import useRegisterForm from "../hooks/useRegisterForm"; */
 
 const RegisterPage = () => {
-  const { registerSubmitHandler } = useContext(AuthContext);
+  /*  const { registerSubmitHandler } = useContext(AuthContext);
   const { values, errors, serverError, onChange, onSubmit } = useRegisterForm(
     registerSubmitHandler
-  );
+  ); */
+
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null)
+      navigate("/login")
+      console.log(data);
+    } catch (err) {
+      setLoading(false);
+      setError(err.message);
+    }
+  };
 
   return (
     <>
       <div className="site-heading">
         <h1 className="heading-title">Create new Account</h1>
       </div>
-      {serverError && (
+       {error && (
         <div className="alert">
-          <h2>{serverError}</h2>
+          <h2>{error}</h2>
         </div>
       )}
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit}>
         <p className="input-box">
           <label htmlFor="email">E-Mail</label>
-          {errors.email && <span className="error">{errors.email}</span>}
+          {/*  {errors.email && <span className="error">{errors.email}</span>} */}
           <input
             type="email"
             name="email"
             id="email"
-            onChange={onChange}
-            value={values?.email}
-            className={errors.email ? "error-input" : ""}
+            onChange={handleChange}
+
+            /* className={errors.email ? "error-input" : ""} */
           />
         </p>
         <p className="input-box">
           <label htmlFor="username">Username</label>
-          {errors.username && <span className="error">{errors.username}</span>}
+          {/*   {errors.username && <span className="error">{errors.username}</span>} */}
 
           <input
             type="text"
             name="username"
             id="username"
-            onChange={onChange}
-            value={values?.username}
-            className={errors.username ? "error-input" : ""}
+            onChange={handleChange}
+
+            /* className={errors.username ? "error-input" : ""} */
           />
         </p>
         <p className="input-box">
           <label htmlFor="password">Password</label>
-          {errors.password && <span className="error">{errors.password}</span>}
+          {/* {errors.password && <span className="error">{errors.password}</span>} */}
 
           <input
             type="password"
             name="password"
             id="password"
-            onChange={onChange}
-            value={values?.password}
-            className={errors.password ? "error-input" : ""}
+            onChange={handleChange}
+
+            /*  className={errors.password ? "error-input" : ""} */
           />
         </p>
         <p className="input-box">
           <label htmlFor="repass">Confirm Password</label>
-          {errors.repass && <span className="error">{errors.repass}</span>}
+          {/*  {errors.repass && <span className="error">{errors.repass}</span>} */}
 
           <input
             type="password"
             name="repass"
             id="repass"
-            onChange={onChange}
-            value={values?.repass}
-            className={errors.repass ? "error-input" : ""}
+            onChange={handleChange}
+
+            /*  className={errors.repass ? "error-input" : ""} */
           />
         </p>
-        <button type="submit" className="btn-1">
-          Create Account
+        <button disabled={loading} type="submit" className="btn-1">
+          {loading ? "Loading..." : "Create Account"}
         </button>
         <p className="btn-2 auth">
           <Link to="/login">Login instead</Link>
