8260691614c65950ab8f46547ac9ada8af43f10b
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-09-12 10:45:00 +0200

    add redux toolkit and redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop.js";
+import { persistor, store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ScrollToTop />
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </BrowserRouter>
 );
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,13 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice.js";
 import "../styles/auth.css";
 import "../styles/form.css";
-import { useState } from "react";
 /* import AuthContext from "../context/authContext";
 import useLoginForm from "../hooks/useLoginForm"; */
 
@@ -11,9 +17,10 @@ const LoginPage = () => {
     useLoginForm(loginSubmitHandler); */
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+
+  const { loading, error } = useSelector((state) => state.user);
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -26,7 +33,7 @@ const LoginPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -35,20 +42,16 @@ const LoginPage = () => {
         body: JSON.stringify(formData),
       });
 
-      console.log(res)
+      console.log(res);
       const data = await res.json();
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate("/");
-      
     } catch (err) {
-      setLoading(false);
-      setError(err.message);
+      dispatch(signInFailure(err.message));
     }
   };
   return (
