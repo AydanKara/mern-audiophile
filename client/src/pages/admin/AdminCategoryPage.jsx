3221007b1682b6bdc6f6519f37ef20d9d9122401
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-09-28 15:34:39 +0200

    Update AdminCategoryPage.jsx

--- client/src/pages/admin/AdminCategoryPage.jsx
+++ client/src/pages/admin/AdminCategoryPage.jsx
@@ -1,48 +1,22 @@
-import { Button, message, Popconfirm, Space, Table } from "antd";
+import {
+  Button,
+  message,
+  Popconfirm,
+  Space,
+  Table,
+  Form,
+  Input,
+  Spin,
+} from "antd";
 import { useCallback, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 const AdminCategoryPage = () => {
   const apiUrl = import.meta.env.VITE_API_BASE_URL; // Your API base URL
   const [dataSource, setDataSource] = useState([]);
+  const [form] = Form.useForm();
+  const [editingCategory, setEditingCategory] = useState(null); // State for editing category
   const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
 
-  const fetchCategories = useCallback(async () => {
-    setLoading(true); // Start loading
-    try {
-      const response = await fetch(`${apiUrl}/api/category`);
-      if (!response.ok) {
-        throw new Error("Failed to fetch categories");
-      }
-      const data = await response.json(); // Parse the JSON from response
-      setDataSource(data); // Set the fetched categories in dataSource
-    } catch (error) {
-      message.error(error.message);
-    } finally {
-      setLoading(false); // Stop loading
-    }
-  }, [apiUrl]); // Add apiUrl as a dependency
-
-  useEffect(() => {
-    fetchCategories(); // Fetch categories when the component mounts
-  }, [fetchCategories]); // Use fetchCategories as a dependency
-
-  const deleteCategory = async (id) => {
-    try {
-      const response = await fetch(`${apiUrl}/api/category/${id}`, {
-        method: "DELETE",
-        credentials: 'include',
-      });
-      if (!response.ok) {
-        throw new Error("Failed to delete category");
-      }
-      message.success("Category deleted successfully");
-      fetchCategories(); // Refetch categories after deletion
-    } catch (error) {
-      message.error(error.message);
-    }
-  };
   const columns = [
     {
       title: "Image",
@@ -62,10 +36,7 @@ const AdminCategoryPage = () => {
       key: "actions",
       render: (_, record) => (
         <Space>
-          <Button
-            type="primary"
-            onClick={() => navigate(`update/${record._id}`)}
-          >
+          <Button type="primary" onClick={() => onEditCategory(record)}>
             Update
           </Button>
           <Popconfirm
@@ -82,13 +53,167 @@ const AdminCategoryPage = () => {
     },
   ];
 
+  const fetchCategories = useCallback(async () => {
+    setLoading(true); // Start loading
+    try {
+      const response = await fetch(`${apiUrl}/api/category`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch categories");
+      }
+      const data = await response.json(); // Parse the JSON from response
+      setDataSource(data); // Set the fetched categories in dataSource
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      setLoading(false); // Stop loading
+    }
+  }, [apiUrl]); // Add apiUrl as a dependency
+
+  const deleteCategory = async (id) => {
+    try {
+      const response = await fetch(`${apiUrl}/api/category/${id}`, {
+        method: "DELETE",
+        credentials: "include",
+      });
+      if (!response.ok) {
+        throw new Error("Failed to delete category");
+      }
+      message.success("Category deleted successfully");
+      fetchCategories(); // Refetch categories after deletion
+    } catch (error) {
+      message.error(error.message);
+    }
+  };
+
+  const onFinish = async (values) => {
+    setLoading(true);
+    try {
+      if (editingCategory) {
+        // Update existing coupon
+        const response = await fetch(
+          `${apiUrl}/api/category/${editingCategory._id}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(values),
+            credentials: "include",
+          }
+        );
+        if (response.ok) {
+          message.success("Category updated successfully");
+          setEditingCategory(null); // Clear the editing state
+          form.resetFields();
+          fetchCategories(); // Refresh the list of categories
+        } else {
+          message.error("Failed to update coupon.");
+        }
+      } else {
+        const response = await fetch(`${apiUrl}/api/category/create`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(values),
+          credentials: "include",
+        });
+        if (response.ok) {
+          message.success("Category created successfully");
+          form.resetFields();
+          fetchCategories(); // Refetch categories after creation
+        }
+      }
+    } catch (error) {
+      console.error("Failed to create category" + error);
+      message.error("Failed to create category");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onEditCategory = (category) => {
+    setEditingCategory(category);
+    form.setFieldsValue({
+      name: category.name,
+      img: category.img,
+    });
+  };
+
+  const onCancelEdit = () => {
+    setEditingCategory(null);
+    form.resetFields();
+  };
+
+  useEffect(() => {
+    fetchCategories(); // Fetch categories when the component mounts
+  }, [fetchCategories]); // Use fetchCategories as a dependency
+
   return (
-    <Table
-      dataSource={dataSource}
-      columns={columns}
-      rowKey={(record) => record._id}
-      loading={loading}
-    />
+    <>
+      <Spin spinning={loading}>
+        <Form
+          form={form}
+          name="basic"
+          layout="inline"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            margin: "1rem 0",
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          autoComplete="off"
+          onFinish={onFinish}
+        >
+          <Form.Item
+            label="Category name"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: "Please input category name!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Image (Url)"
+            name="img"
+            rules={[
+              {
+                required: true,
+                message: "Please input image url",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Button type="primary" htmlType="submit">
+            {editingCategory ? "Update Category" : "Create Category"}
+          </Button>
+          {editingCategory && (
+            <Button onClick={onCancelEdit} style={{ marginLeft: "8px" }}>
+              Cancel
+            </Button>
+          )}
+        </Form>
+      </Spin>
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        rowKey={(record) => record._id}
+        loading={loading}
+      />
+    </>
   );
 };
 
