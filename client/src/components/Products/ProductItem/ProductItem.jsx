af0b4b6160e73174e9ac0042d7aa87253b3cb822
AydanKara <113279073+AydanKara@users.noreply.github.com>
2024-11-10 00:35:32 +0100

    update ProductItem Component
    add responsive styles

--- client/src/components/Products/ProductItem/ProductItem.jsx
+++ client/src/components/Products/ProductItem/ProductItem.jsx
@@ -1,29 +1,53 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import "./ProductItem.css";
+import { useEffect, useState } from "react";
 
 const ProductItem = ({ product, isReversed }) => {
+  const [isReversedActive, setIsReversedActive] = useState(() => {
+    // Initial state check for window size
+    return window.innerWidth > 768 ? isReversed : false;
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Automatically runs when the component re-renders due to state change
+      setIsReversedActive(window.innerWidth > 768 ? isReversed : false);
+    };
+
+    handleResize(); // Run the check on component mount
+
+    // Instead of adding a listener, re-run `handleResize` when the window size changes due to rerenders
+    const resizeObserver = new ResizeObserver(handleResize);
+    resizeObserver.observe(document.body);
+
+    // Cleanup when component unmounts
+    return () => resizeObserver.disconnect();
+  }, [isReversed]);
   return (
     <li className="product-item">
       <article>
-        <div className={`product-wrapper ${isReversed ? "reversed" : ""}`}>
-          {isReversed ? (
+        <div
+          className={`products-wrapper ${isReversedActive ? "reversed" : ""}`}
+        >
+          {isReversedActive ? (
             <>
               <div className="col-2">
-                <div className="product-info">
+                <div className={`product-info padding-right`}>
                   <h2>{product.name}</h2>
                   <p className="product-desc">{product.description}</p>
-                  <Link
-                    to={`/product/${product._id}`}
-                    className="btn-1"
-                  >
+                  <Link to={`/product/${product._id}`} className="btn-1">
                     See Product
                   </Link>
                 </div>
               </div>
               <div className="col-1">
                 <div className="product-image">
-                  <img src={product.image} alt="" />
+                  <img
+                    src={product.image}
+                    srcSet={`${product.imageTablet} 768w`}
+                    alt=""
+                  />
                 </div>
               </div>
             </>
@@ -31,17 +55,18 @@ const ProductItem = ({ product, isReversed }) => {
             <>
               <div className="col-1">
                 <div className="product-image">
-                  <img src={product.image} alt="" />
+                  <img
+                    src={product.image}
+                    srcSet={`${product.imageTablet} 768w`}
+                    alt=""
+                  />
                 </div>
               </div>
               <div className="col-2">
                 <div className="product-info">
                   <h2>{product.name}</h2>
                   <p className="product-desc">{product.description}</p>
-                  <Link
-                    to={`/product/${product._id}`}
-                    className="btn-1"
-                  >
+                  <Link to={`/product/${product._id}`} className="btn-1">
                     See Product
                   </Link>
                 </div>
